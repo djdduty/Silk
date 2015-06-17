1561a788f67a2be0443216db60443497fbfcfdae
Jordan Duty <djdduty@gmail.com>
2015-06-16 23:30:12 -0700

    Merge conflicts fixed

--- TestClient/src/main.cpp
+++ TestClient/src/main.cpp
@@ -9,15 +9,11 @@
     #include <Windows.h>
 #endif
 
-<<<<<<< HEAD
 #include <Silk.h>
 
-int main(int ArgC,char *ArgV[])
-=======
 using namespace TestClient;
 
 int main(int ArgC,char ArgV[])
->>>>>>> b0808e305c71ff0ad013f5f2c5c6f9949586f030
 {
     Window* Win = new Window();
     Win->Create(Vec2(800,600), "Test silk Client");
